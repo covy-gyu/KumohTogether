6cd89e2bee1b01a6b39517a49fad83f2a25b6562
guchur <74587816+gozldwnd1eo@users.noreply.github.com>
2022-05-29 00:05:11 +0900

    [Mod] 콜백 사용으로 비동기 처리

--- server/DBController/UserController.ts
+++ server/DBController/UserController.ts
@@ -1,56 +1,60 @@
 import { ServerError } from 'colyseus';
 import { createConnection } from 'mysql';
-import { IUser } from '../../types/Users'
+import mysql from 'mysql';
+import { IUser } from '../../types/Users';
 
 const connection = createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '1234',
-    database : 'kumoh_together'
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'kumoh_together'
 })
 
-let id: string 
-let password: string
-let result: boolean
-let msg: string 
-let code: number
 
-export default function login(user: IUser) {
-    id = user.id
-    password = user.password
-    result = user.result
-    msg = user.msg
-    code = user.code
+export default function login(message:{id:string,password:string}, callback: (user:IUser)=>void) {
+ 
+    const user = {
+        id: message.id,
+        password : message.password,
+        result : false,
+        msg : '',
+        code: 0,
+    }
+
     var json
-    const conn = connection.connect();
-    connection.query('SELECT * from user where user_id = ' + id, (error: any, rows: string) => {
+    connection.connect;
+    connection.query('SELECT * from kumoh_together.member where member_id = ' + mysql.escape(user.id) + ';', (error: any, rows: string) => {
         if (error) throw error;
+
+        
+        
         var string = JSON.stringify(rows);
         json = JSON.parse(string);
-
         if (json[0]) {
             //조회된 행이 있을때(id 존재)
-            if(json.user_password === password){
-                
-                result = true
+            if (json[0].member_password === user.password) {
+
+                user.result = true
                 console.log("로그인 성공")
             }
-            else{
-                
-                throw new ServerError(403, '비밀번호가 틀렸습니다!')
+            else {
+
+                console.log('비밀번호가 틀렸습니다!')
             }
 
         }
         else {
-            throw new ServerError(403, '아이디가 존재하지 않습니다!')
+            console.log('아이디가 존재하지 않습니다!')
 
         }
 
-        console.log(json[0].user_id);
-        console.log(json[0].user_password);
+        // console.log(json[0].member_id);
+        // console.log(json[0].member_password);
+        callback(user)
+
     })
-    connection.end();
-    return result
+    // await connection.end();
+
 }
 
 
